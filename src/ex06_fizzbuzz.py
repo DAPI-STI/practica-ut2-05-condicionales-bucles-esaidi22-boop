b1559e2d0f2ec18822f64c6c5bb7b557dd1b542d
esaidi22-boop <esaidi22@educacion.navarra.es>
2026-01-12 12:21:49 +0100

    ex6

--- src/ex06_fizzbuzz.py
+++ src/ex06_fizzbuzz.py
@@ -11,7 +11,19 @@ def fizzbuzz(n: int) -> list[str]:
     - Si es múltiplo de 5: "Buzz"
     - Si es múltiplo de 3 y 5: "FizzBuzz"
     - En otro caso: el número en texto (por ejemplo "7")
-
     Si n <= 0, devuelve lista vacía.
     """
-    raise NotImplementedError("Implementa fizzbuzz(n)")
+    if n <= 0:
+        return []
+
+    resultado = []
+    for i in range(1, n + 1):
+        if i % 15 == 0:           # múltiplos de 3 y 5
+            resultado.append("FizzBuzz")
+        elif i % 3 == 0:
+            resultado.append("Fizz")
+        elif i % 5 == 0:
+            resultado.append("Buzz")
+        else:
+            resultado.append(str(i))
+    return resultado
